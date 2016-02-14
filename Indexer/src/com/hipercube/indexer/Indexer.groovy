8fc7edbd6b019564c7b1b0ab14b84ce0b25c7fbf
Joowon Ryoo <joowonryoo@gmail.com>
2016-02-14 16:26:14 +0900

    Implement Indexer

--- Indexer/src/com/hipercube/indexer/Indexer.groovy
+++ Indexer/src/com/hipercube/indexer/Indexer.groovy
@@ -1,5 +1,13 @@
 package com.hipercube.indexer
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer
+import org.apache.lucene.document.Document
+import org.apache.lucene.document.Field
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.store.Directory
+import org.apache.lucene.store.FSDirectory
+import org.apache.lucene.util.Version
+
 /**
  * Copyright (c) 2/7/16 Joowon Ryoo
  * <p>
@@ -36,5 +44,55 @@ class Indexer {
         long start = System.currentTimeMillis()
         Indexer indexer = new Indexer(indexDir)
         int numIndexed
+
+        try {
+            numIndexed = indexer.index(dataDir, new TextFilesFilter())
+        } finally {
+            indexer.close()
+        }
+
+        long end = System.currentTimeMillis()
+
+        System.out.println("Indexing ${numIndexed} files took ${end - start} milliseconds")
+    }
+
+
+    private IndexWriter writer
+
+    Indexer(String indexDir) throws IOException {
+        Directory dir = FSDirectory.open(new File(indexDir))
+        writer = new IndexWriter(dir, new StandardAnalyzer(Version.LUCENE_30), true, IndexWriter.MaxFieldLength.UNLIMITED)
+    }
+
+    void close() throws IOException { writer.close() }
+
+    int index(String dataDir, FileFilter filter) throws Exception {
+        File[] files = new File(dataDir).listFiles()
+
+        for (File f : files) {
+            if (!f.isDirectory() && !f.isHidden() && f.exists() && f.canRead() && (filter == null || filter.accept(f)))
+                indexFile(f)
+        }
+
+        return writer.numDocs()
+    }
+
+    private static class TextFilesFilter implements FileFilter {
+        @Override
+        boolean accept(File pathname) { pathname.getName().toLowerCase().endsWith(".txt") }
+    }
+
+    protected Document getDocument(File f) throws Exception {
+        Document doc = new Document()
+        doc.add(new Field("contents", new FileReader(f)))
+        doc.add(new Field("filename", f.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED))
+        doc.add(new Field("fullpath", f.getCanonicalPath(), Field.Store.YES, Field.Index.NOT_ANALYZED))
+        return doc
+    }
+
+    private void indexFile(File f) throws Exception {
+        System.out.println("Indexing " + f.getCanonicalPath())
+        Document doc = getDocument(f)
+        writer.addDocument(doc)
     }
 }
